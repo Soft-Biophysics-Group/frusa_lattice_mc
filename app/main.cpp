9c422070dca50959ef44c285b311859ed163b5f8
Andrey Zelenskiy <andrey.zelenskiy@universite-paris-saclay.fr>
2024-03-01 15:51:06 +0100

    Implemented and tested CMake for generating the executable

--- app/main.cpp
+++ app/main.cpp
@@ -1,18 +1,21 @@
-#include "model.h"
-#include "mc_routines.h"
+//#include "model.h"
+//#include "mc_routines.h"
+#include <iostream>
 
 int main(){
 
-  model_space::model new_model = model_space::initialize_model();
-  
-  simulation_space::mc_params mc_params_1d;
-  
-  new_model.print_state();
-  new_model.print_energy();
 
-  simulation_space::mc<model_space::model> annealing(new_model, mc_params_1d);
+  std::cout << "Program runs!\n";
+  //model_space::model new_model = model_space::initialize_model();
+  
+  //simulation_space::mc_params mc_params_1d;
+  
+  //new_model.print_state();
+  //new_model.print_energy();
 
-  annealing.t_scan();
+  //simulation_space::mc<model_space::model> annealing(new_model, mc_params_1d);
+
+  //annealing.t_scan();
 
   return 0;
 }

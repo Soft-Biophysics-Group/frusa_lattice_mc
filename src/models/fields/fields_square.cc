795be619df90db38d08a5df83e19e9b58acbc94e
Andrey <andrey.zelenskiy@universite-paris-saclay.fr>
2024-03-09 22:57:01 +0100

    Fixed a segmentation fault error in vector allocation

--- src/models/fields/fields_square.cc
+++ src/models/fields/fields_square.cc
@@ -9,20 +9,29 @@ namespace fields_space{
     // Read the coupling matrix for the bond along the +x axis from the input
     // file. The structure of couplings is assumed to be a flattened version of
     // the coupling_matrix.
+    vec2d array_0;
     for(int a=0;a<4;a++){
+      vec1d row;
       for(int b=0;b<4;b++){
-        coupling_matrix[0][a][b] = couplings[1+a+4*b];
+        row.push_back(couplings[1+a+4*b]);
       }
+      array_0.push_back(row);
     }
 
+    coupling_matrix.push_back(array_0);
+
     for(int k=1;k<4;k++){
+      vec2d array_k;
       for(int a=0;a<4;a++){
+        vec1d row;
         for(int b=0;b<4;b++){
           int ap = array_space::mod(a-k,4);
           int bp = array_space::mod(a+k,4);
-          coupling_matrix[k][a][b] = coupling_matrix[0][ap][bp];
+          row.push_back(coupling_matrix[0][ap][bp]);
         }
+        array_k.push_back(row);
       }
+      coupling_matrix.push_back(array_k);
     }
 
     return coupling_matrix; 

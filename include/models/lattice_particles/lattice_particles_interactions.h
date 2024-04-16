f5aea8c0f217ec820068bfbc92ab39c8245b9fa7
Vincent Ouazan-Reboul <vincent.ouazan@gmail.com>
2024-04-16 15:19:53 +0200

    Cleans up interactions

--- include/models/lattice_particles/lattice_particles_interactions.h
+++ include/models/lattice_particles/lattice_particles_interactions.h
@@ -1,31 +1,33 @@
 #ifndef LATTICEPARTICLES_INTERACTIONS_H
 #define LATTICEPARTICLES_INTERACTIONS_H
 
+#include "lattice_particles_geometry.h"
 #include "lattice_particles_parameters.h"
 #include "lattice_particles_state.h"
-#include "lattice_particles_geometry.h"
 
-#include <iostream>
-#include <cmath>
 #include <array>
+#include <cmath>
+#include <iostream>
 
 #include "vector_utils.h"
 
 namespace lattice_particles_space {
-// TODO FIgure out, after going thru the code, if this struct is actually
-// necessary!
+
 // Structure containing the characteristics of the model interactions:
 // couplings          - Interaction energy between neighbouring particles
-// energy             - Energy of the system
-
+// energy             - Total energy of the system
+// neighbours         - Array of indices neighbouring a given site, updated at
+//                      each energy calculation. Useful to avoid constant
+//                      allocations.
+// n_edges:           - number of neighbours of lattice sites; hopefully
+//                      something I can remove in the future.
 struct interactions_struct {
-    ContactMap couplings {};
-    double energy {};
-    Neighbours neighbours {};
-    int n_edges {};
+  ContactMap couplings{};
+  double energy{};
+  Neighbours neighbours{};
+  int n_edges{};
 };
 
-// Calculate interactions characteristics of the current state of the system
 void initialize_interactions(state_struct &state,
                              interactions_struct &interactions,
                              model_parameters_struct &parameters);
@@ -33,31 +35,34 @@ void initialize_interactions(state_struct &state,
 // Print the summary of the interactions characteristics
 void print_interactions(state_struct &state, interactions_struct &interactions);
 
+// Print current system energy
 void print_energy(state_struct &state, interactions_struct &interactions);
 
 // Get the contact energy between 2 neighbouring sites with indices site1 and
 // site2, touching along respective edge indices edge1 and edge2.
-// contact_map is the object containing the energy of all possible contacts
-// between pairs of particles.
 double get_contact_energy(state_struct &state, int site1, int site2,
                           interactions_struct &interactions);
 
+// Get total energy of a given site, which is the sum of contact energy with
+// its neighbours
 double get_site_energy(state_struct &state, interactions_struct &interactions,
                        int site_index);
 
-double get_energy(state_struct& state, interactions_struct& interactions);
+// Get total energy of the system
+double get_energy(state_struct &state, interactions_struct &interactions);
 
-// This function takes as the reference edge for the contact the smallest
-// edge and assigns a contact identity based on it.
-int get_contact_index(state_struct &state, int site1, int site2, int edge1,
-                      interactions_struct& interactions);
+// Returns the contact index for two sites, which is the coefficient of the
+// corresponding contact in the coupling vector
+int get_contact_index(state_struct &state, int site1, int site2,
+                      interactions_struct &interactions);
 
 int get_contact_index_full_empty(int edge, int type, int orientation,
                                  int n_edges);
+
 int get_contact_index_full_full(int state1, int state2, int edge1, int n_states,
                                 int n_edges, int n_types);
 
-std::ostream& operator<< (std::ostream& out, interactions_struct& interactions); 
-} // lattice_particles_space
+std::ostream &operator<<(std::ostream &out, interactions_struct &interactions);
+} // namespace lattice_particles_space
 
 #endif
--- src/models/lattice_particles/lattice_particles_interactions.cc
+++ src/models/lattice_particles/lattice_particles_interactions.cc
@@ -12,18 +12,14 @@ void initialize_interactions(state_struct &state,
                              interactions_struct &interactions,
                              model_parameters_struct &parameters) {
   interactions.couplings = parameters.couplings;
-  interactions.n_edges =
-      static_cast<int>(std::size(interactions.neighbours));
+  interactions.n_edges = static_cast<int>(std::size(interactions.neighbours));
   interactions.energy = get_energy(state, interactions);
   interactions.neighbours = Neighbours();
 }
 
 double get_contact_energy(state_struct &state, int site1, int site2,
                           interactions_struct &interactions) {
-  // Note the +1: get_bond_direction returns values starting at 0, and our
-  // edges run from 1 to n_edges, as 0 is empty
-  int edge1{get_bond_direction(site1, site2, state)+1};
-  //std::cout << "Edge between neighbours is " << edge1 << '\n' ;
+  // std::cout << "Edge between neighbours is " << edge1 << '\n' ;
 
   // An empty-empty contact counts as zero energy
   if (state.lattice_sites.is_empty(site1) and
@@ -32,18 +28,19 @@ double get_contact_energy(state_struct &state, int site1, int site2,
   }
 
   int contact_index{
-      get_contact_index(state, site1, site2, edge1, interactions)};
+      get_contact_index(state, site1, site2, interactions)};
 
-  //std::cout << "Index of corresponding contact is " << contact_index << '\n' ;
+  // std::cout << "Index of corresponding contact is " << contact_index << '\n'
+  // ;
 
   return interactions.couplings[static_cast<std::size_t>(contact_index)];
 }
 
 int get_contact_index_full_empty(int edge, int type, int orientation,
                                  int n_edges) {
-  //std::cout << "Full-empty contact\n"
-            //<< "(type " << type << ", orientation " << orientation << ", edge "
-            //<< edge << ")\n";
+  // std::cout << "Full-empty contact\n"
+  //<< "(type " << type << ", orientation " << orientation << ", edge "
+  //<< edge << ")\n";
   return type * n_edges + (std::abs(edge - orientation) % n_edges);
 }
 
@@ -56,22 +53,27 @@ int get_contact_index_full_full(int state1, int state2, int edge1, int n_states,
   // Note we apply an offset of n_edges*n_orientations because the first
   // componentes of the interaction vector are reserved for surface tension
   // terms
-  //std::cout << "Full-full contact\n"<<
-    //"(state1: " << state1 << ", state2 " << state2 << ", edge1: " << edge1 << ")\n";
+  // std::cout << "Full-full contact\n"<<
+  //"(state1: " << state1 << ", state2 " << state2 << ", edge1: " << edge1 <<
+  //")\n";
   if (edge1 <= n_edges / 2) {
     return n_types * n_edges - 1 +
            array_space::hash_3integers(state1, state2, edge1, n_states,
-                                       n_edges/2);
+                                       n_edges / 2);
   } else {
     return n_types * n_edges - 1 +
-           array_space::hash_3integers(
-               state2, state1, get_conjugate_edge(edge1), n_states, n_edges/2);
+           array_space::hash_3integers(state2, state1,
+                                       get_conjugate_edge(edge1), n_states,
+                                       n_edges / 2);
   }
 }
 
-int get_contact_index(state_struct &state, int site1, int site2, int edge1,
+int get_contact_index(state_struct &state, int site1, int site2,
                       interactions_struct &interactions) {
   int contact_index{0};
+  // Note the +1: get_bond_direction returns values starting at 0, and our
+  // edges run from 1 to n_edges, as 0 is empty
+  int edge1{get_bond_direction(site1, site2, state) + 1};
   // The first n_edges*n_types coefficients of the LEL are for surface tension
   // terms.
   // Coefficients 0 to n_states-1 are for the surface tensions of edges of
@@ -80,12 +82,12 @@ int get_contact_index(state_struct &state, int site1, int site2, int edge1,
   if (state.lattice_sites.is_empty(site1)) {
     contact_index = get_contact_index_full_empty(
         get_conjugate_edge(edge1), state.lattice_sites.get_type(site2),
-        state.lattice_sites.get_orientation(site2), interactions.n_edges);}
-  else if (state.lattice_sites.is_empty(site2)) {
+        state.lattice_sites.get_orientation(site2), interactions.n_edges);
+  } else if (state.lattice_sites.is_empty(site2)) {
     contact_index = get_contact_index_full_empty(
         edge1, state.lattice_sites.get_type(site1),
-        state.lattice_sites.get_orientation(site1), interactions.n_edges);}
-  else {
+        state.lattice_sites.get_orientation(site1), interactions.n_edges);
+  } else {
     int state1{state.lattice_sites.get_state(site1)};
     int state2{state.lattice_sites.get_state(site2)};
     contact_index =
@@ -97,35 +99,35 @@ int get_contact_index(state_struct &state, int site1, int site2, int edge1,
 
 double get_site_energy(state_struct &state, interactions_struct &interactions,
                        int site_index) {
-  //std::cout << "\nGetting energy of site " << site_index << '\n' ;
+  // std::cout << "\nGetting energy of site " << site_index << '\n' ;
   double site_energy{0.0};
   get_neighbours(interactions.neighbours, site_index, state);
   // NOTE that the loop index is also the contact edge for the site @
   // site_index
   // TODO Check that's actually the case...
   for (int neighbour_site : interactions.neighbours) {
-    //std::cout << "Checking neighbour " << neighbour_site << '\n' ;
-    // TODO Check I'm looking at the right directions
+    // std::cout << "Checking neighbour " << neighbour_site << '\n' ;
+    //  TODO Check I'm looking at the right directions
     site_energy +=
         get_contact_energy(state, site_index, neighbour_site, interactions);
   }
   return site_energy;
 }
 
-double get_energy(state_struct &state, interactions_struct& interactions) {
+double get_energy(state_struct &state, interactions_struct &interactions) {
   double energy{0.0};
   for (int i{0}; i < state.n_sites; i++)
     energy += get_site_energy(state, interactions, i);
   // Divide by 2, otherwise we'll be double counting!
-  //std::cout << "Energy is now " << energy / 2 ;
+  // std::cout << "Energy is now " << energy / 2 ;
   return energy / 2;
 }
 
-std::ostream& operator<< (std::ostream& out, interactions_struct& interactions) {
-  out << "Printing interactions structure\n" ;
+std::ostream &operator<<(std::ostream &out, interactions_struct &interactions) {
+  out << "Printing interactions structure\n";
   out << "Printing coupling matrix: ";
   array_space::print_vector(out, interactions.couplings);
-  out << '\n' ;
+  out << '\n';
   out << "System energy is: " << interactions.energy << '\n';
   out << "Last verified neighbours were: ";
   array_space::print_array<int, 6>(out, interactions.neighbours);
@@ -134,12 +136,12 @@ std::ostream& operator<< (std::ostream& out, interactions_struct& interactions) 
   return out;
 }
 
-void print_interactions(state_struct &state, interactions_struct &interactions) {
+void print_interactions(state_struct &state,
+                        interactions_struct &interactions) {
   std::cout << interactions;
 }
 
-
 void print_energy(state_struct &state, interactions_struct &interactions) {
-  std::cout << interactions.energy ;
+  std::cout << interactions.energy;
 }
 } // namespace lattice_particles_space

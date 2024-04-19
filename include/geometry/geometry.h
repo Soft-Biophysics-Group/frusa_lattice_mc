c9b1422e89d885f9cc433879e33256b0847d347f
Vincent Ouazan-Reboul <vincent.ouazan@gmail.com>
2024-04-19 18:08:26 +0200

    Starts writing triangular lattice

--- include/geometry/geometry.h
+++ include/geometry/geometry.h
@@ -3,22 +3,11 @@
 
 #include "vector_utils.h"
 #include <array>
-template <int M, int N>
-using arr2i = std::array<std::array<int, N>,M>;
 
 namespace geometry_space {
 // Vectors of vectors giving the permutation indices for all the possible bond
 // orientations.
-struct bond_permutation_struct {
-  static constexpr arr2i<6, 6> hexagonal{{{1, 2, 3, 4, 5, 6},
-                                          {2, 3, 4, 5, 6, 1},
-                                          {3, 4, 5, 6, 1, 2},
-                                          {4, 5, 6, 1, 2, 3},
-                                          {5, 6, 1, 2, 3, 4},
-                                          {6, 1, 2, 3, 4, 5}}};
-};
-
-enum class lattices {
+enum class lattice_options {
   one_dimension,
   square,
   triangular,
@@ -31,14 +20,14 @@ enum class lattices {
 class Geometry {
 public:
   Geometry() = default;
-  Geometry(lattices lattice, int lx, int ly, int lz);
+  Geometry(lattice_options lattice, int lx, int ly, int lz=1);
   int get_neighbour(const int site_ind, const int bond_ind) const;
   int get_bond(const int site_1_ind, const int site_2_ind) const;
   int get_interaction_indices(const int site_1_ind, const int site_2_ind) const;
   double get_interaction(const int site_1_ind, const int site_2_ind,
                          const vec1d couplings) const;
 private:
-  lattices lattice_m {lattices::one_dimension};
+  lattice_options lattice_m {lattice_options::one_dimension};
   int lx_m {1};
   int ly_m {1};
   int lz_m {1};
--- include/geometry/triangular.h
+++ include/geometry/triangular.h
@@ -1,21 +1,24 @@
 #ifndef GEOMETRY_TRIANGULAR_H
 #define GEOMETRY_TRIANGULAR_H
 
+#include "vector_utils.h"
+#include <unordered_map>
 namespace geometry_space {
-
-namespace triangular_space {
-
-class triangular_lattice {
-public:
-  triangular_lattice(int lx, int ly, int n_orientations);
-
-private:
-  int n_orientations_m{};
-  int n_edges_m{6};
-  int l_x_m{};
-  int l_y_m{};
+struct bond_permutation_struct {
+  static constexpr arr2i<6, 6> bond_permutations{{{1, 2, 3, 4, 5, 6},
+                                                  {2, 3, 4, 5, 6, 1},
+                                                  {3, 4, 5, 6, 1, 2},
+                                                  {4, 5, 6, 1, 2, 3},
+                                                  {5, 6, 1, 2, 3, 4},
+                                                  {6, 1, 2, 3, 4, 5}}};
+  static inline const std::unordered_map<vec1i, int> bond_indices{
+      {{1, 0}, 0},  {{1, 1}, 1},   {{-1, 1}, 2},
+      {{-1, 0}, 3}, {{-1, -1}, 4}, {{1, -1}, 5}};
 };
 
+namespace triangular_space {
+int get_neighbour_triangular(const int site_ind, const int bond_ind,
+                             const int lx, const int ly);
 } // namespace triangular_space
 
 } // namespace geometry_space

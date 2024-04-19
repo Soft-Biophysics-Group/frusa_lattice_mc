befc02e8dd6cc161ad0a5cd31adb5917923fb9fe
Andrey Zelenskiy <andrey.zelenskiy@universite-paris-saclay.fr>
2024-04-19 17:34:46 +0200

    initialized files for chain, square and fcc lattices

---
+++ include/geometry/square.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2024 Soft Biophysics Group LPTMS
+// Part of frusa_mc, released under BSD 3-Clause License.
+
+#ifndef SQUARE_GEOMETRY_HEADER_H
+#define SQUARE_GEOMETRY_HEADER_H
+
+#include <iostream>
+
+namespace geometry_space {
+  namespace square_space {
+  
+  } // square_space
+} // geometry_space
+ 
+#endif //SQUARE_GEOMETRY_HEADER_H

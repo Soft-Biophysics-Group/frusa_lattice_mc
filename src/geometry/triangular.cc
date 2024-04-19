288532f239d161c9b838108450cda1ad9c265a44
Vincent Ouazan-Reboul <vincent.ouazan@gmail.com>
2024-04-19 18:09:56 +0200

    Fixes small typo

--- src/geometry/triangular.cc
+++ src/geometry/triangular.cc
@@ -2,44 +2,43 @@
 #include "geometry.h"
 
 namespace geometry_space {
-  namespace triangular_space {
-  int get_neighbour_triangular(const int site_ind, const int bond_ind,
-                               const int lx, const int ly) {
+namespace triangular_space {
+int get_neighbour_triangular(const int site_ind, const int bond_ind,
+                             const int lx, const int ly) {
 
-    int i{};
-    int j{};
-    int neigh_ind {0};
+  int i{};
+  int j{};
+  int neigh_ind{0};
 
-    array_space::r_to_ij(site_ind, i, j, lx, ly);
+  array_space::r_to_ij(site_ind, i, j, lx, ly);
 
-    int im = array_space::mod(i - 1, lx);
-    int ip = array_space::mod(i + 1, lx);
-    int jm = array_space::mod(j - 1, ly);
-    int jp = array_space::mod(j + 1, ly);
+  int im = array_space::mod(i - 1, lx);
+  int ip = array_space::mod(i + 1, lx);
+  int jm = array_space::mod(j - 1, ly);
+  int jp = array_space::mod(j + 1, ly);
 
-    switch (bond_ind) {
-    case 0:
-      array_space::ij_to_r(neigh_ind, ip, j, lx, ly);
-      break;
-    case 1:
-      array_space::ij_to_r(neigh_ind, ip, jp, lx, ly);
-      break;
-    case 2:
-      array_space::ij_to_r(neigh_ind, i, jp, lx, ly);
-      break;
-    case 3:
-      array_space::ij_to_r(neigh_ind, im, j, lx, ly);
-      break;
-    case 4:
-      array_space::ij_to_r(neigh_ind, im, jm, lx, ly);
-      break;
-    case 5:
-      array_space::ij_to_r(neigh_ind, i, jm, lx, ly);
-      break;
-    }
-
-    return neigh_ind;
+  switch (bond_ind) {
+  case 0:
+    array_space::ij_to_r(neigh_ind, ip, j, lx, ly);
+    break;
+  case 1:
+    array_space::ij_to_r(neigh_ind, ip, jp, lx, ly);
+    break;
+  case 2:
+    array_space::ij_to_r(neigh_ind, i, jp, lx, ly);
+    break;
+  case 3:
+    array_space::ij_to_r(neigh_ind, im, j, lx, ly);
+    break;
+  case 4:
+    array_space::ij_to_r(neigh_ind, im, jm, lx, ly);
+    break;
+  case 5:
+    array_space::ij_to_r(neigh_ind, i, jm, lx, ly);
+    break;
   }
-  }
-  } // triangular_space
-  } // geometry_space
+
+  return neigh_ind;
+}
+} // namespace triangular_space
+} // namespace geometry_space

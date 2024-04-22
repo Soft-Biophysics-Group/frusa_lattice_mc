cf4fb2c51d06b9451a9d370bed30c3e49eea3cdb
Andrey Zelenskiy <andrey.zelenskiy@universite-paris-saclay.fr>
2024-04-22 18:19:48 +0200

    Added bond structures for chain, square, and fcc lattices

--- include/geometry/chain.h
+++ include/geometry/chain.h
@@ -9,6 +9,30 @@
 namespace geometry_space {
   namespace chain_space {
   
+    struct bond_structure {
+      bond_structure();
+      
+      constexpr unsigned int bond_permutation[2][2] = {
+        { 0, 1 },
+        { 1, 0 }
+      }; // bond_permutation
+
+      constexpr int bond_array[2][3] = {
+        {  1, 0, 0 },
+        { -1, 0, 0 }
+      }; // bond_array
+
+      std::map<int[3], unsigned int> bond_index {
+        { {  1, 0, 0 }, 0 },
+        { { -1, 0, 0 }, 1 },
+      }; // bond_index
+    }; // bond_structure
+
+    void get_bond();
+    void get_neighbour();
+    void get_interaction();
+    void get_interaction_indices();
+
   } // chain_space
 } // geometry_space
  

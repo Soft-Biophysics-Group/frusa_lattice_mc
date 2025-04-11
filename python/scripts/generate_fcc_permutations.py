14adc374816cee930eb908ce5d7f60958a387b0d
Vincent Ouazan-Reboul <vincent.ouazan@gmail.com>
2025-04-11 22:16:56 +0200

    Adds script to generate FCC permutations

---
+++ python/scripts/generate_fcc_permutations.py
@@ -0,0 +1,30 @@
+"""
+Vincent Ouazan-Reboul, 2025
+Script to generate the orientation permutations when viewed under different bonds for the
+`include/geometry/fcc.h` file.
+The function you want to use is `output_formatted_bonds(output_file)`, which puts in a text file
+the bond permutations as they will be formatted in the `fcc.h` header.
+By default, this script outputs the results in `python/data/geometry/fcc.txt`. To change
+output file, go to the last line of the script and give a file location as argument for
+`output_formatted_bonds(output_file)`.
+"""
+
+import config as cfg
+from geometry import ParticleGeometry
+
+fcc_geometry = ParticleGeometry.from_lattice_name("fcc")
+
+def format_bond_permutations():
+    all_bond_permutations = fcc_geometry.gen_bond_permutations()
+    permutations_str = "{\n"
+    for permutations in all_bond_permutations:
+        permutations_str += "{" + str(permutations)[1:-1] + "},\n"
+    permutations_str += "}"
+    return permutations_str
+
+def output_formatted_bonds(output_path = cfg.python_path/"data/geometry/fcc.txt"):
+    with open(output_path, "w") as f:
+        f.write(format_bond_permutations())
+    return
+
+output_formatted_bonds()

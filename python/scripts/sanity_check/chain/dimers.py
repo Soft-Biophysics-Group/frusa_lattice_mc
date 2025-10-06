acbe8cdb78e6aeb61cf27197d4da3ca4d7563b57
Vincent Ouazan-Reboul <vincent.ouazan@gmail.com>
2025-10-06 13:00:35 +0200

    Trims off fat on Python code

--- python/scripts/sanity_check/chain/dimers.py
+++
@@ -1,6 +0,0 @@
-from helper_fct import sanity_check_oned
-
-# This program should get us a crystal, where all particles form a single
-# aggregate and contact between all possible pairs of faces happen
-
-sanity_check_oned(10.0, -10.0, 10.0)

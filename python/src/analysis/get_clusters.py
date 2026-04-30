06db6a2a1bf3cc1536008545b7017b1b9b6c8f78
Vincent Ouazan-Reboul <vincent.ouazan@gmail.com>
2026-04-30 18:51:57 +0200

    Adds functions to measure crystalline domain sizes

--- python/src/analysis/get_clusters.py
+++ python/src/analysis/get_clusters.py
@@ -71,6 +71,60 @@ def get_aggregate_sizes(aggregates: Aggregates) -> list[int]:
     return [len(agg) for agg in aggregates]
 
 
+def get_crystalline_domains(
+    struct_index: int = -1,
+    struct_folder: str | Path = "",
+    struct_file: str | Path = "",
+    model_file: str | Path = cfg.default_mc_params_file,
+) -> Aggregates:
+    """
+    Returns a list of sets. Each member is a crystalline domain, i.e. a set of connected particles with the same orientation.
+    If struct_file is specified: overrides directly fetches results in
+    struct_file.
+    If struct_index is specified: load structure with index struct_index from folder
+    struct_folder.
+    If not, laods final structure from struct_folder.
+    """
+
+    site_orientations = cfg.load_structure(
+        struct_index=struct_index, struct_folder=struct_folder, struct_file=struct_file
+    )
+    full_sites = cfg.get_full_sites(site_orientations)
+    full_sites_set = set(full_sites)
+    lattice, _ = geometry_from_model_file(model_file)
+
+    visited_sites = set()
+    to_visit = set()
+    all_crystalline_components = []
+
+    for site in full_sites:
+        if site not in visited_sites:
+            to_visit.add(site)
+            cryst_comp = set()
+            ref_orientation = site_orientations[1, site]
+            # We build each cluster iteratively, shell by shell, by going through the neighbours
+            # of each site and adding them to the cluster until we run out of them
+            while len(to_visit) > 0:
+                # Build first shell of the cluster
+                site_to_visit = to_visit.pop()
+                orientation_site_to_visit = site_orientations[1, site_to_visit]
+                if orientation_site_to_visit == ref_orientation:
+                    cryst_comp.add(site_to_visit)
+                    visited_sites.add(site_to_visit)
+                    neighbour_sites = lattice.get_neighbour_sites(site_to_visit)
+                    full_neighbour_sites = full_sites_set.intersection(
+                        set(neighbour_sites)
+                    )
+                    # This is a set substraction: only keeps non-visited sites in the full neighbour
+                    # sites
+                    neighbours_to_visit = full_neighbour_sites - visited_sites
+                    # Updates the to_visit set with its intersection with neighbours_to_visit
+                    to_visit |= neighbours_to_visit
+            all_crystalline_components.append(cryst_comp)
+
+    return all_crystalline_components
+
+
 def get_n_interfaces_w_ext(
     aggregates: Aggregates,
     model_file: str | Path = cfg.default_mc_params_file,

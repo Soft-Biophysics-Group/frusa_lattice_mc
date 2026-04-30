43245dbc300e84cf6496452b12a0d8772ad04b21
Vincent Ouazan-Reboul <vincent.ouazan@gmail.com>
2026-04-30 18:48:38 +0200

    Adds function to measure squared energy

--- python/src/analysis/analyze_records.py
+++ python/src/analysis/analyze_records.py
@@ -7,6 +7,7 @@ For now, just one function: analyze the energy records of our MC simulations
 
 from pathlib import Path
 import numpy as np
+from numpy.typing import NDArray
 
 # Plotting the energy trace to see when the cube actually formed
 def get_temperature_energy_records(
@@ -41,4 +42,19 @@ def get_average_energies(averages_folder: str | Path = ""):
     all_T_sorted = np.array(all_T)[sorting_inds]
     all_e_sorted = np.array(all_avg_e)[sorting_inds]
 
-    return all_T_sorted, np.hstack(all_e_sorted)
+    return all_T_sorted, all_e_sorted
+
+def get_average_squared_energies(averages_folder: str | Path = ""):
+    all_T = []
+    all_avg_sq_e = []
+    avgs_path = Path(averages_folder)
+    for f in avgs_path.iterdir():
+        this_avg = np.loadtxt(f)
+        all_T.append(this_avg[0])
+        all_avg_sq_e.append(this_avg[2])
+    # Sort by decreasing order of temperature
+    sorting_inds = np.argsort(all_T)[::-1]
+    all_T_sorted = np.array(all_T)[sorting_inds]
+    all_e_sorted:NDArray[np.float64] = np.array(all_avg_sq_e)[sorting_inds]
+
+    return all_T_sorted, all_e_sorted

7ed675a22bb317bc000fb7d1fdd5dbd3c1ac2adc
Vincent Ouazan-Reboul <vincent.ouazan@gmail.com>
2026-07-17 19:15:26 +0200

    Refactors analysis module

--- python/src/frusa_lattice_mc/analysis/analyze_records.py
+++ python/src/frusa_lattice_mc/analysis/analyze_records.py
@@ -9,66 +9,34 @@ from pathlib import Path
 import numpy as np
 from numpy.typing import NDArray
 
+class Averages:
+    temperatures: NDArray[np.float64]
+    energies: NDArray[np.float64]
+    squared_energies: NDArray[np.float64]
+    autocorr_times: NDArray[np.float64]
 
-# Plotting the energy trace to see when the cube actually formed
-def get_temperature_energy_records(records_folder: str | Path = ""):
-    all_T = []
-    all_e_records = []
-    records_path = Path(records_folder)
-    for f in records_path.iterdir():
-        this_record = np.loadtxt(f)
-        all_T.append(this_record[0])
-        all_e_records.append(this_record[1:])
-    # Sort by decreasing order of temperature
-    sorting_inds = np.argsort(all_T)[::-1]
-    all_T_sorted = np.array(all_T)[sorting_inds]
-    all_T_sorted = np.repeat(all_T_sorted, len(all_e_records[0]))
-    all_e_sorted = np.array(all_e_records)[sorting_inds]
+    def __init__(self, averages_folder: Path | str):
+        all_T = []
+        all_e_records = []
+        all_e_sq_records = []
+        all_autocorr_times = []
 
-    return all_T_sorted, np.hstack(all_e_sorted)
+        averages_path = Path(averages_folder)
+        for f in averages_path.glob("esf_av_T_*.dat"):
+            this_record = np.loadtxt(f)
+            all_T.append(this_record[0])
+            all_e_records.append(this_record[1])
+            all_e_sq_records.append(this_record[2])
+            all_autocorr_times.append(this_record[3])
 
-def get_average_energies_and_squared(averages_folder: str | Path = ""):
-    all_T = []
-    all_avg_e = []
-    all_avg_sq_e = []
-    avgs_path = Path(averages_folder)
-    for f in sorted(avgs_path.iterdir()):
-        this_avg = np.loadtxt(f)
-        all_T.append(this_avg[0])
-        all_avg_e.append(this_avg[1])
-        all_avg_sq_e.append(this_avg[2])
-    sorting_inds = np.argsort(all_T)[::-1]
-    all_T_sorted = np.array(all_T)[sorting_inds]
-    all_e_sorted = np.array(all_avg_e)[sorting_inds]
-    all_sq_e_sorted = np.array(all_avg_sq_e)[sorting_inds]
-    return all_T_sorted, all_e_sorted, all_sq_e_sorted
+        # Sort by decreasing order of temperature
+        sorting_inds = np.argsort(all_T)[::-1]
+        self.temperatures = np.array(all_T)[sorting_inds]
+        self.energies = np.array(all_e_records)[sorting_inds]
+        self.squared_energies = np.array(all_e_sq_records)[sorting_inds]
+        self.autocorr_times = np.array(all_autocorr_times)[sorting_inds]
 
-def get_average_energies(averages_folder: str | Path = ""):
-    all_T = []
-    all_avg_e = []
-    avgs_path = Path(averages_folder)
-    for f in avgs_path.iterdir():
-        this_avg = np.loadtxt(f)
-        all_T.append(this_avg[0])
-        all_avg_e.append(this_avg[1])
-    # Sort by decreasing order of temperature
-    sorting_inds = np.argsort(all_T)[::-1]
-    all_T_sorted = np.array(all_T)[sorting_inds]
-    all_e_sorted = np.array(all_avg_e)[sorting_inds]
-
-    return all_T_sorted, all_e_sorted
-
-def get_average_squared_energies(averages_folder: str | Path = ""):
-    all_T = []
-    all_avg_sq_e = []
-    avgs_path = Path(averages_folder)
-    for f in avgs_path.iterdir():
-        this_avg = np.loadtxt(f)
-        all_T.append(this_avg[0])
-        all_avg_sq_e.append(this_avg[2])
-    # Sort by decreasing order of temperature
-    sorting_inds = np.argsort(all_T)[::-1]
-    all_T_sorted = np.array(all_T)[sorting_inds]
-    all_e_sorted:NDArray[np.float64] = np.array(all_avg_sq_e)[sorting_inds]
-
-    return all_T_sorted, all_e_sorted
+    @property
+    def heat_capacity(self):
+        variance = np.maximum(self.squared_energies - self.energies**2, 0.0)
+        return variance / self.temperatures**2

a9ee53fdb2164f63aaef7682f91997f8526a2e12
Vincent Ouazan-Reboul <vincent.ouazan@gmail.com>
2026-07-17 17:16:49 +0200

    Updates designs to put all contacts in the canonical form

--- python/src/frusa_lattice_mc/designs/cubic.py
+++ python/src/frusa_lattice_mc/designs/cubic.py
@@ -1,26 +1,31 @@
-""" Vincent Ouazan-Rebo, 2025
+""" Vincent Ouazan-Reboul, 2025
 Storing the contact map designs for cubic lattice particles.
 """
 
 from ..contact_utils import ContactMapWrapper
+from ..geometry.particle_geometry import CubicParticle
 
-CRYSTAL_CONTACTS = [(4 * i, (4 * i + 12) % 24) for i in range(3)]
-CRYSTAL_CONTACTS_ONE_AXIS = [
-    (0, 12),
-    (0, 13),
-    (0, 14),
-    (0, 15),
-    (4, 6),
-    (4, 11),
-    (4, 16),
-    (4, 21),
-    (8, 8),
-    (8, 19),
-    (8, 20),
-    (16, 18),
-    (16, 23),
-    (20, 20),
-]
+CRYSTAL_CONTACTS = CubicParticle().get_all_canonical_contacts(
+    [(4 * i, (4 * i + 12) % 24) for i in range(3)]
+)
+CRYSTAL_CONTACTS_ONE_AXIS = CubicParticle().get_all_canonical_contacts(
+    [
+        (0, 12),
+        (0, 13),
+        (0, 14),
+        (0, 15),
+        (4, 6),
+        (4, 11),
+        (4, 16),
+        (4, 21),
+        (8, 8),
+        (8, 19),
+        (8, 20),
+        (16, 18),
+        (16, 23),
+        (20, 20),
+    ]
+)
 ALL_TRUE_CAMEMBERT = {
     "Rminus_flag": True,
     "Gminus_flag": True,
@@ -29,24 +34,26 @@ ALL_TRUE_CAMEMBERT = {
     "Gplus_flag": True,
     "Bplus_flag": True,
 }
-HEDGEHOG_CONTACTS = [
-    (12, 4),
-    (12, 5),
-    (12, 6),
-    (12, 7),
-    (12, 8),
-    (12, 9),
-    (12, 10),
-    (12, 11),
-    (12, 16),
-    (12, 17),
-    (12, 18),
-    (12, 19),
-    (12, 20),
-    (12, 21),
-    (12, 22),
-    (12, 23),
-]
+HEDGEHOG_CONTACTS = CubicParticle().get_all_canonical_contacts(
+    [
+        (12, 4),
+        (12, 5),
+        (12, 6),
+        (12, 7),
+        (12, 8),
+        (12, 9),
+        (12, 10),
+        (12, 11),
+        (12, 16),
+        (12, 17),
+        (12, 18),
+        (12, 19),
+        (12, 20),
+        (12, 21),
+        (12, 22),
+        (12, 23),
+    ]
+)
 # HEDGEHOG_CONTACTS = [
 #     (4, 12),
 #     (4, 13),
@@ -70,100 +77,45 @@ HEDGEHOG_CONTACTS = [
 #     (20, 14),
 #     (20, 15),
 # ]
-EXTRA_HEDGEHOG_DEFECT_CONTACTS = [(12, 12), (12, 13), (12, 14), (12, 15)]
+EXTRA_HEDGEHOG_DEFECT_CONTACTS = CubicParticle().get_all_canonical_contacts(
+    [(12, 12), (12, 13), (12, 14), (12, 15)]
+)
 
 
-def set_contacts(
-    cmap: ContactMapWrapper, contact_list: list[tuple[int, int]], value: float
-):
-    for contact in contact_list:
-        cmap[*contact] = value
-    return
-
-def set_crystal_contacts(crystal_e: float, cmap: ContactMapWrapper):
-    for contact in CRYSTAL_CONTACTS:
-        cmap[*contact] = crystal_e
-    return
-
-def set_crystal_one_axis_contacts(crystal_e: float, cmap: ContactMapWrapper):
-    set_contacts(cmap, CRYSTAL_CONTACTS_ONE_AXIS, crystal_e)
-
-
-def set_triple_vortex_camembert_contacts(
-    defect_e: float,
-    cmap: ContactMapWrapper,
+def get_triple_vortex_camembert_contacts(
     contact_flags: dict[str, bool] = ALL_TRUE_CAMEMBERT,
-):
-    contacts_Rplus = [(4, 13), (8, 13), (12, 17), (12, 23)]
-    contacts_Gplus = [(0, 17), (8, 16), (12, 17), (16, 20)]
-    contacts_Bplus = [(0, 21), (4, 22), (12, 23), (16, 20)]
-    contacts_Rminus = [(0, 9), (0, 17), (0, 21), (0, 7)]
-    contacts_Gminus = [(4, 22), (4, 13), (4, 10), (0, 7)]
-    contacts_Bminus = [(0, 9), (4, 10), (8, 13), (8, 16)]
+) -> frozenset[tuple[int, int]]:
+    contacts_Rplus = CubicParticle().get_all_canonical_contacts(
+        [(4, 13), (8, 13), (12, 17), (12, 23)]
+    )
+    contacts_Gplus = CubicParticle().get_all_canonical_contacts(
+        [(0, 17), (8, 16), (12, 17), (16, 20)]
+    )
+    contacts_Bplus = CubicParticle().get_all_canonical_contacts(
+        [(0, 21), (4, 22), (12, 23), (16, 20)]
+    )
+    contacts_Rminus = CubicParticle().get_all_canonical_contacts(
+        [(0, 9), (0, 17), (0, 21), (0, 7)]
+    )
+    contacts_Gminus = CubicParticle().get_all_canonical_contacts(
+        [(4, 22), (4, 13), (4, 10), (0, 7)]
+    )
+    contacts_Bminus = CubicParticle().get_all_canonical_contacts(
+        [(0, 9), (4, 10), (8, 13), (8, 16)]
+    )
 
-    camembert_contacts = []
+    camembert_contacts = set()
     if contact_flags["Rplus_flag"]:
-        camembert_contacts.extend(contacts_Rplus)
+        camembert_contacts |= contacts_Rplus
     if contact_flags["Gplus_flag"]:
-        camembert_contacts.extend(contacts_Gplus)
+        camembert_contacts |= contacts_Gplus
     if contact_flags["Bplus_flag"]:
-        camembert_contacts.extend(contacts_Bplus)
+        camembert_contacts |= contacts_Bplus
     if contact_flags["Rminus_flag"]:
-        camembert_contacts.extend(contacts_Rminus)
+        camembert_contacts |= contacts_Rminus
     if contact_flags["Gminus_flag"]:
-        camembert_contacts.extend(contacts_Gminus)
+        camembert_contacts |= contacts_Gminus
     if contact_flags["Bminus_flag"]:
-        camembert_contacts.extend(contacts_Bminus)
+        camembert_contacts |= contacts_Bminus
 
-    for contact in camembert_contacts:
-        cmap[*contact] = defect_e
-    return
-
-def gen_triple_vortex_camembert_contacts(crystal_e:float, defect_e:float, mismatch_e:float):
-    cmap = ContactMapWrapper.cubic(1, mismatch_e)
-    set_crystal_contacts(crystal_e, cmap)
-    set_triple_vortex_camembert_contacts(defect_e, cmap)
-
-    return cmap
-
-
-def set_hedgehog_camembert_contacts(defect_e: float, cmap: ContactMapWrapper):
-    for contact in HEDGEHOG_CONTACTS:
-        cmap[*contact] = defect_e
-
-    return 
-
-def set_hedgehog_camembert_contacts_w_extra(defect_e: float, cmap: ContactMapWrapper):
-    for contact in HEDGEHOG_CONTACTS:
-        cmap[*contact] = defect_e
-    for contact in EXTRA_HEDGEHOG_DEFECT_CONTACTS:
-        cmap[*contact] = defect_e
-
-    return 
-
-def gen_hedgehog_camembert_cmap(crystal_e:float, defect_e:float, mismatch_e:float):
-    cmap = ContactMapWrapper.cubic(1, mismatch_e)
-    set_crystal_contacts(crystal_e, cmap)
-    set_hedgehog_camembert_contacts(defect_e, cmap)
-
-    return cmap
-
-
-def gen_hedgehog_camembert_cmap_crystal_one_axis(
-    crystal_e: float, defect_e: float, mismatch_e: float
-):
-    cmap = ContactMapWrapper.cubic(1, mismatch_e)
-    set_crystal_one_axis_contacts(crystal_e, cmap)
-    set_hedgehog_camembert_contacts(defect_e, cmap)
-
-    return cmap
-
-
-def gen_hedgehog_camembert_cmap_extra_contacts(
-    crystal_e: float, defect_e: float, mismatch_e: float
-):
-    cmap = ContactMapWrapper.cubic(1, mismatch_e)
-    set_crystal_one_axis_contacts(crystal_e, cmap)
-    set_hedgehog_camembert_contacts_w_extra(defect_e, cmap)
-
-    return cmap
+    return frozenset(camembert_contacts)
--- python/src/frusa_lattice_mc/designs/fcc.py
+++ python/src/frusa_lattice_mc/designs/fcc.py
@@ -1,98 +1,108 @@
 """Vincent Ouazan-Reboul, 2025
 Storing the contact map designs for fcc lattice particles.
 """
+from typing import Union
 
 from ..contact_utils import ContactMapWrapper
+from ..geometry.particle_geometry import FccParticle
 
-CRYSTAL_CONTACTS: list[tuple[int, int]] = [(2 * i, (2 * i + 12) % 24) for i in range(6)]
+CRYSTAL_CONTACTS = FccParticle().get_all_canonical_contacts(
+    [(2 * i, (2 * i + 12) % 24) for i in range(6)]
+)
 
 RED_MINUS_FACES = [10, 11, 12, 13, 16, 17, 20, 21]
 ORTHOGONAL_RED_FACES = [2, 3, 6, 7, 14, 15, 18, 19]
-HEDGEHOG_CONTACTS = [
+HEDGEHOG_CONTACTS = FccParticle().get_all_canonical_contacts([
     (face_1, face_2)
     for face_1 in RED_MINUS_FACES
     for face_2 in RED_MINUS_FACES
     if face_1 != face_2
-] + [(face_1, face_2) for face_1 in RED_MINUS_FACES for face_2 in ORTHOGONAL_RED_FACES]
+] + [(face_1, face_2) for face_1 in RED_MINUS_FACES for face_2 in ORTHOGONAL_RED_FACES])
 
 
-SEED_CORNER = [
-    (10, 11),
-    (10, 21),
-    (10, 12),
-    (10, 17),
-    (12, 13),
-    (12, 16),
-    (12, 20),
-    (16, 21),
-    (16, 17),
-    (20, 21),
-]
+SEED_CORNER = FccParticle().get_all_canonical_contacts(
+    [
+        (10, 11),
+        (10, 21),
+        (10, 12),
+        (10, 17),
+        (12, 13),
+        (12, 16),
+        (12, 20),
+        (16, 21),
+        (16, 17),
+        (20, 21),
+    ]
+)
 
-CRYSTAL_INTS_CORNER = [
-    (0, 21),
-    (0, 12),
-    (0, 17),
-    (0, 11),
-    (4, 20),
-    (4, 13),
-    (4, 16),
-    (4, 10),
-    (20, 22),
-    (12, 23),
-    (16, 22),
-    (10, 22),
-    (8, 20),
-    (8, 13),
-    (8, 16),
-    (8, 10),
-    (6, 18),
-    (18, 18),
-    (14, 19),
-    (14, 14),
-    (6, 15),
-    (2, 14),
-    (2, 7),
-    (2, 2),
-    (2, 19),
-    (6, 6),
-]
+CRYSTAL_INTS_CORNER = FccParticle().get_all_canonical_contacts(
+    [
+        (0, 21),
+        (0, 12),
+        (0, 17),
+        (0, 11),
+        (4, 20),
+        (4, 13),
+        (4, 16),
+        (4, 10),
+        (20, 22),
+        (12, 23),
+        (16, 22),
+        (10, 22),
+        (8, 20),
+        (8, 13),
+        (8, 16),
+        (8, 10),
+        (6, 18),
+        (18, 18),
+        (14, 19),
+        (14, 14),
+        (6, 15),
+        (2, 14),
+        (2, 7),
+        (2, 2),
+        (2, 19),
+        (6, 6),
+    ]
+)
 
 
-SEED_HOLE_INTS_CORNER = [
-    (2, 10),
-    (2, 11),
-    (2, 12),
-    (2, 13),
-    (2, 16),
-    (2, 17),
-    (2, 20),
-    (2, 21),
-    (6, 10),
-    (6, 11),
-    (6, 12),
-    (6, 13),
-    (6, 16),
-    (6, 17),
-    (6, 20),
-    (6, 21),
-    (10, 14),
-    (10, 19),
-    (10, 14),
-    (10, 15),
-    (11, 19),
-    (12, 14),
-    (12 ,15),
-    (12, 18),
-    (12, 19),
-    (14, 16),
-    (14, 17),
-    (14, 20),
-    (14, 21),
-    (16, 19),
-    (17, 19),
-    (18, 20),
-    (18, 21),
-]
+SEED_HOLE_INTS_CORNER = FccParticle().get_all_canonical_contacts(
+    [
+        (2, 10),
+        (2, 11),
+        (2, 12),
+        (2, 13),
+        (2, 16),
+        (2, 17),
+        (2, 20),
+        (2, 21),
+        (6, 10),
+        (6, 11),
+        (6, 12),
+        (6, 13),
+        (6, 16),
+        (6, 17),
+        (6, 20),
+        (6, 21),
+        (10, 14),
+        (10, 19),
+        (10, 14),
+        (10, 15),
+        (11, 19),
+        (12, 14),
+        (12, 15),
+        (12, 18),
+        (12, 19),
+        (14, 16),
+        (14, 17),
+        (14, 20),
+        (14, 21),
+        (16, 19),
+        (17, 19),
+        (18, 20),
+        (18, 21),
+    ]
+)
 
-DEFECT_CORNER = SEED_CORNER + SEED_HOLE_INTS_CORNER
+DEFECT_CORNER = SEED_CORNER | SEED_HOLE_INTS_CORNER
